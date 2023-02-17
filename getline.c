5c3c65f7c8f59049d65d7f310edbff3cdc9d77e7
kahbeer42 <olabodex2m@gmail.com>
2023-02-16 21:37:56 -0800

    updated

--- getline.c
+++ getline.c
@@ -17,8 +17,7 @@ ssize_t _getline(char **lineptr, size_t *n, FILE *stream);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
 	void *mem;
-	char *ptr_copy;
-	char *filler;
+	char *ptr_copy, *filler;
 	unsigned int index;
 
 	if (new_size == old_size)
